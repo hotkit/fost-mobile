c7a919e2bb16fac131a7d4b94f8c6155ae494535
Kirit Sælensminde <k@kirit.com>
2015-01-09 13:22:23 +0700

    Switch to C++14.

--- opts.mk
+++ opts.mk
@@ -1,5 +1,7 @@
 
-LOCAL_CFLAGS += -DBOOST_VERSION_MAJOR=56 -std=c++11 \
+LOCAL_CFLAGS += \
+    -DBOOST_VERSION_MAJOR=56 \
+    -std=c++14 \
     -Wno-deprecated-declarations
 
 LOCAL_ARM_MODE := arm
